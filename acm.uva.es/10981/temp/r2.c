ad898f8644f820db8aea3a4fd822bf336895af53
Ivan Krasilnikov <infnty@gmail.com>
2011-06-21 02:42:57 +0400

    Clean up

--- acm.uva.es/10981/temp/r2.c
+++
@@ -1,7 +0,0 @@
-main(i,j,t){
-  printf("1000\n");
-  for(t=0;t<1000;t++){
-    for(i=0;i<5;i++)putchar('a'+(rand()%3));putchar('\n');
-    printf("%c\n",'a'+(rand()%3));
-  }
-}
--- uva-icpclive/3014.dhaka04-c/postproc/2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	char buf[10000];
-	while (gets(buf))
-		if (buf[0]) puts(buf);
-}
